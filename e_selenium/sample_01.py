fb4fd05cad12ef8e9b938d725cf304df050e10c8
linian365boy <linian365boy@foxmail.com>
2018-07-31 22:46:40 +0800

    refactor(craw): xls modify to csv

--- e_selenium/sample_01.py
+++ e_selenium/sample_01.py
@@ -1,14 +1,25 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
 
-driver.get("http://www.python.org")
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
+driver.get("https://www.baidu.com")
+elem = driver.find_element_by_id("kw")
+elem.send_keys("mysql")
+
+time.sleep(5)
+
+driver.find_element_by_xpath('//*[@id="su"]').click()
 elem.send_keys(Keys.RETURN)
 
-result = driver.get_screenshot_as_file("/Users/linian/xxx.png")
+time.sleep(5)
+
+for i in range(18):
+    js = "window.scrollTo(0,{})".format(i * 500)
+    driver.execute_script(js)
+    time.sleep(0.3)
+result = driver.get_screenshot_as_file("/Users/niange/xxx.png")
 print(result)
 driver.close()
