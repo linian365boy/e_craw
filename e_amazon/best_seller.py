926d05ebecb31c879a636271171207dce3d97050
nian.li <nian.li@shopee.com>
2018-08-20 10:40:33 +0800

    feat(craw): add some chrome param

--- e_amazon/best_seller.py
+++ e_amazon/best_seller.py
@@ -51,6 +51,9 @@ logger = logging.getLogger('simple')
 chrome_options = Options()
 # define headless
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 header = random.choice(headers_list)
 
